aff201e8eb33d135c6ba3ac9bda7db0556e6e0da
Carmen Romano <my-9@hotmail.it>
2024-05-28 08:44:12 +0200

    test con debugger

--- src/Esercizio0/Debugger.java
+++ src/Esercizio0/Debugger.java
@@ -1,7 +1,20 @@
 package Esercizio0;
 
+import java.util.Scanner;
+
 public class Debugger {
     public static void main(String[] args) {
+        int[] numeri={1,2,3,4,5,6,7,8,9};
+        for (int i = 0; i < numeri.length; i++) {
+            System.out.println(numeri[i]); }
 
-}
-}
+            Scanner scanner = new Scanner(System.in);
+
+            int numero = scanner.nextInt();
+
+            System.out.println("Hai inserito il numero: " + numero);
+
+            scanner.close();
+        }
+    }
+
